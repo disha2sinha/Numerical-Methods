0af12015774b580b768082f9ef4c52eab29e1219
Disha Sinha <42354197+disha2sinha@users.noreply.github.com>
2019-07-11 16:37:10 +0530

    Add files via upload

---
+++ Newton_forwardInterpolation.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fact(int n){
+	if(n==1)
+	{
+		return(1);
+	}
+	else{
+		return(n*fact(n-1));
+	}
+}
+void main()
+{
+	float a[20][20],sum=0,u,h,x,temp=1;
+	int i,j,n;
+	printf("\n HOW MANY NUMBERS?\n");
+	scanf("%d",&n);
+	
+	
+	
+}
