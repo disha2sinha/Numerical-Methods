b85990f5d74cf27c87d5fea6fc301d3f66cc2afd
Disha Sinha <42354197+disha2sinha@users.noreply.github.com>
2019-07-13 21:36:04 +0530

    Add files via upload

---
+++ Lagrange_interpolation.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	float x[20],y[20],sum=0,x1,temp=1;
+	int i,j,n;
+	printf("\n HOW MANY NUMBERS?\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("\nENTER x VALUES:\n");
+		scanf("%f",&x[i]);
+		printf("\nENTER y VALUES:");
+		scanf("%f",&y[i]);
+	}
+	printf("\nENTER NUMBER:\n");
+	scanf("%f",&x1);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i==j)
+			{
+				continue;
+				temp*=(x1-x[j])/(x[i]-x[j]);
+			}
+		}
+		sum+=temp*y[i];
+		printf("t=%f,s=%f\n",temp,sum);
+		temp=1;
+	}
+	printf("\nf(%f)=%f",x1,sum);
+}
